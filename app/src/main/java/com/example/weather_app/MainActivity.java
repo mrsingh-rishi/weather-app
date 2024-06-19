2182c6303b0b87d2f92cc6fbf067c326c3025caf
Rishi Singh <rs3949427@gmail.com>
2024-06-19 22:28:17 +0530

    UI Completed

--- app/src/main/java/com/example/weather_app/MainActivity.java
+++ app/src/main/java/com/example/weather_app/MainActivity.java
@@ -3,6 +3,16 @@ package com.example.weather_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.*;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+import org.json.JSONObject;
+import java.io.*;
+import java.net.*;
+import java.util.concurrent.ExecutionException;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +21,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    
 }
